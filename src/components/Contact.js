0ca9b0e95b8cc36850c8185a1a0d5a5cc29499e3
vikash1311 <gautam7.ven@gmail.com>
2026-04-06 19:09:30 +0530

    contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ export default function Contact() {
     setStatus('sending');
     setErrMsg('');
     try {
-      const res = await fetch(`${API_URL}/api/contact`, {
+      const res = await fetch(`${API_URL}/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
